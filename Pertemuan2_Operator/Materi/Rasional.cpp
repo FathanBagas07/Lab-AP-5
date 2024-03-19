4db0b655c0923e8cd4d09367e144abf5e188d785
unknown <fatan250217@gmail.com>
2024-03-19 23:56:53 +0700

    Second Commit

---
+++ Pertemuan2_Operator/Materi/Rasional.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    system ("cls");
+
+    int bilangan1 = 10;
+    int bilangan2 = 20;
+
+    // Sama dengan
+    // bool hasil = bilangan1 == bilangan2;
+
+    // cout << hasil;
+
+    // Tidak sama dengan
+    bool hasil = bilangan1 != bilangan2;
+
+    cout << hasil;
+
+
+    return 0;
+}
---
+++ Pertemuan2_Operator/Materi/Ternerry.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    system ("cls");
+
+    bool status = true;
+
+    string hasil = status? "Benar" : "Salah";
+
+    cout << hasil;
+
+    return 0;
+}
